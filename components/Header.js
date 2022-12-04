80d9d52cbc43b932cf31e0f9107fb25d95e8652e
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2022-12-04 15:13:37 +0700

    Initial Commit

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <div className="headerContainer">
+            <style jsx>{`
+                .headerContainer {
+                    width: 100%;
+                    height: 80px;
+                }
+            `}</style>
+        </div>
+    );
+};
+
+export default Header;
